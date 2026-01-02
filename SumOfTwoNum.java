2f00924ffb08f5d8f6d1561783f3daef9732df2d
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-02 12:48:46 +0000

    Jan 2, 2026, 6:18 PM

---
+++ SumOfTwoNum.java
@@ -0,0 +1,15 @@
+public class SumOfTwoNum 
+{
+    static int add(int a, int b)
+    {
+        int add = a+b;
+        return add;
+    }
+    public static void main(String[] args)
+    {
+        int a= 20;
+        int b= 10;
+        int result = add( a ,b);
+        System.out.println("Result is: " + result);
+    }
+}
