aa1f89949b9193223bda6ca90c953244f78f3a21
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-02 18:20:25 +0000

    Jan 2, 2026, 11:50 PM

---
+++ DynamicInput_calculator.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class DynamicInput_calculator 
+{
+
+    static int add( int a, int b)
+    {
+        return a+b;
+    }
+    static int sub( int a, int b)
+    {
+        return a-b;
+    }
+    static int prod(int a, int b)
+    {
+
+        if( a==0 || b == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return a*b;
+        }
+    }
+    static int div( int a, int b)
+    {
+        if( b == 0)
+        {
+            System.out.println("Invalid choice of b dor division");
+            return 0;
+        }
+        else
+        {
+            return a/b;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a= sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b= sc.nextInt();
+        int addition =add(a, b);
+        System.out.println(addition); 
+    }
+}
