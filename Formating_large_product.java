1519f026790ec14fa423b68b7fcf4cfc5e32df16
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-09 20:14:03 +0000

    Jan 10, 2026, 1:44 AM

--- Formating_large_product.java
+++ Formating_large_product.java
@@ -4,7 +4,7 @@ public class Formating_large_product
     static void product(int a, int b)
     {
         int p=1;
-        for(int i=1; (a+i)<b; i++)
+        for(int i=0; (a+i)<b; i++)
         {
             p = p*(a+i);
             System.out.println("      "+p);
