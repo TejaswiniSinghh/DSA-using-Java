c49a676f8c03dccaf9ca480574c5e03157586174
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-09 20:19:54 +0000

    Jan 10, 2026, 1:49 AM

--- Formating_large_product.java
+++ Formating_large_product.java
@@ -3,14 +3,23 @@ public class Formating_large_product
 {
     static void product(int a, int b)
     {
-        int p=1;
+        int p=1, e=0,d=0;
         for(int i=0; (a+i)<b; i++)
         {
             p = p*(a+i);
-            System.out.println("      "+p);
         }
         p=p*b;
-        System.out.println(p);
+        while(p>0)
+        {
+            if(p%10==0)
+            {
+                e+=1;
+                p=p/10;
+                d=p;
+            }
+            else break;
+        }
+        System.out.println(d+" "+e);
 
 
     }
