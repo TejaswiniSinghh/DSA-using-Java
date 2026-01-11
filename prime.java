2eae7ca62cafd2a430c8cf4beaee0cc2f727f534
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-11 19:29:49 +0000

    Jan 12, 2026, 12:59 AM

---
+++ prime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;public class prime 
+{
+    static void cal(int n)
+    {
+        if (n==1)
+        {
+            System.out.println("nor prime nor composite");
+            for(int i=2; i<=n; i++)
+            {
+                for(int j=2; j<=i;j++)
+                {
+                    int rem=i%j;
+                    if( rem==0 )
+                    {
+                        System.out.print(i+" ");
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        cal(n);
+        sc.close();
+    }
+}
