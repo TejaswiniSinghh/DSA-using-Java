51783b8931c4e0fccb3f5a86bd231bd83011b7ab
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-11 15:56:24 +0000

    Jan 11, 2026, 9:26 PM

--- Pattern_with_space.java
+++ Pattern_with_space.java
@@ -5,7 +5,7 @@ public class Pattern_with_space
     {
         for(int i=n ; i>0; i--)
         {
-            for( int j=0; j<n; j++)
+            for( int j=0; j<i-1; j++)
             {
                 System.out.print(" ");
             }
