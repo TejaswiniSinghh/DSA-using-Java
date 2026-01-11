b676db0ff0510416183baceed782ea234ec6cfa6
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-11 20:06:52 +0000

    Jan 12, 2026, 1:36 AM

---
+++ Print_matrix.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Print_matrix 
+{
+    static void printing(int[][] mat)
+    {
+        for(int i=0; i<mat.length; i++)
+        {
+            for(int j=0; j<mat[0].length; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int[][] mat= new int[2][2];
+        for(int i=0; i<2;i++)
+        {
+            for(int j=0; j<2;j++)
+            {
+                mat[i][j]=sc.nextInt();
+            }
+            System.out.println();
+        }
+        printing( mat);
+    }
+    
+}
