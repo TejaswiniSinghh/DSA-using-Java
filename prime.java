0f0ede85942729e7a71d8b506dd9e701b64bdd7e
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-11 19:44:38 +0000

    Jan 12, 2026, 1:14 AM

--- prime.java
+++ prime.java
@@ -10,7 +10,7 @@ import java.util.Scanner;public class prime
         int count=0;
         for(int i=1; i<=n; i++)
         {
-            for(int j=1; j<=i;j++)
+            for(int j=1; j<=i; j++)
             {
                 int rem= i % j;
                 if( rem==0)
