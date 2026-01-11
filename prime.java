07e35641b16f43db3ea783a11d20fc1a51b32c52
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-11 19:45:36 +0000

    Jan 12, 2026, 1:15 AM

--- prime.java
+++ prime.java
@@ -7,9 +7,9 @@ import java.util.Scanner;public class prime
             System.out.println("nor prime nor composite");
             
         }
-        int count=0;
         for(int i=1; i<=n; i++)
         {
+            int count=0;
             for(int j=1; j<=i; j++)
             {
                 int rem= i % j;
