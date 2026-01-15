8c4ea651827b8a5a88157cfaead0dffe2d64240f
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-16 00:19:51 +0530

    java Code

---
+++ EquilateralTriangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
