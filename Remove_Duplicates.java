b25405a3eb5ef5f93ad58d02d9aad9c0bdb82d43
Tejaswini singh <1128.tejaswini@gmail.com>
2026-01-28 20:25:29 +0530

    java code

--- Remove_Duplicates.java
+++ Remove_Duplicates.java
@@ -18,21 +18,19 @@ public class Remove_Duplicates
     public static void main(String[] args)
     {
         Scanner sc= new Scanner(System.in);
-        System.out.println("Size of array:");
+        System.out.print("Size of array:");
         int size= sc.nextInt();
         int[] nums= new int[size];
         for(int i=0; i< size; i++)
         {
             nums[i]=sc.nextInt();
         }
-        System.out.println("\n");
         System.out.println("Enter value to be checked and removed:");
-        int val= sc.nextInt();
-        System.out.println("\n");
+        int val= sc.nextInt(); 
         int k= removeElement(nums,val);
         for(int i=0; i< k; i++)
         {
-            System.out.print(nums[i] );
+            System.out.print(nums[i] +" " );
         }
     }
 
