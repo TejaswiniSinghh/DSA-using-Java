e23bd64ff38b87047e3f2a269d031bb90bb3aa25
Tejaswini Singh <161447982+TejaswiniSinghh@users.noreply.github.com>
2026-07-08 09:49:28 +0530

    Time: 0 ms (100%), Space: 42 MB (72.34%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) 
+    {
+        int first=1;
+        int second=2;
+        int max=0;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        for(int i=3; i<=n; i++)
+        {
+            int current= first + second;
+            first= second;
+            second= current;
+        }
+        return second;
+    }
+}
